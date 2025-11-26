5896aa83efda20f6b75bf8f602364824b447b6ad
Eize Boonstra <eizekboonstra@gmail.com>
2025-11-26 11:52:59 +0100

    Add population and GDP data CSV files and update Model.py to read them

--- Model.py
+++ Model.py
@@ -2,5 +2,12 @@ import numpy as np
 import scipy as sp
 import pandas as pd
 
-A=pd.read_csv('DemandMatrix.csv',header=None)
-print(A[0][1])
+Demand=pd.read_csv('DemandMatrix.csv',header=None)
+print(Demand[0][1])
+
+pop = pd.read_csv('pop.csv') # "City", "2021", "2024"
+
+print(pop['2021'][0])
+
+GDP = pd.read_csv('GDP.csv') # "City", "2021", "2024"
+print(GDP['2021'][0])
